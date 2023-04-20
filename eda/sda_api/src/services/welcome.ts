b132552cde3b884f5e42640d58183e8853e7fc8a
Juanjo Ortilles <juanjo.ortilles@sportium.com>
2023-04-20 17:35:20 +0200

    X

--- eda/sda_api/src/services/welcome.ts
+++
@@ -1,8 +0,0 @@
-import { NextFunction, Request, Response } from 'express';
-
-export class welcome {
-
-    static async welcome(req: Request, res: Response, next: NextFunction) {
-        return res.status(200).json({ 'status': 'ok'});
-    }
-}
