713c51cc12a5560f5d99c5e165a0c4ae6f60c280
Juanjo Ortilles <juanjo.ortilles@sportium.com>
2023-05-12 05:35:14 +0200

    token

--- eda/eda_api/lib/guards/updateModel-guard.ts
+++ eda/eda_api/lib/guards/updateModel-guard.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { HttpException } from '../module/global/model/index';
-
+import { MD5 } from 'object-hash';
+const SEED = require('../../config/seed').SEED;
 
 
 export const updateModelGuard = async function (req: Request, res: Response, next: NextFunction) {
@@ -11,6 +12,10 @@ export const updateModelGuard = async function (req: Request, res: Response, nex
         console.log(e);
         return next(new HttpException(403, 'valid authentication is requiered'));
     }
+
+    const dia =  new Date();
+    let token = dia.getFullYear( ) +  SEED +  dia.getDay()  + dia.getHours();
+    token = MD5(token);
     // aqui tenemos que implemetar una validación
     if ( updateToken == 'x1' ) {
             next();
