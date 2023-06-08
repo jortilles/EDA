3d65aa1a4743676f006a4fa5efcecbbf40c7b542
Asaf <asaf@jortilles.com>
2023-06-08 17:40:11 +0200

    Revert "Reemplazo oid por id en los ds que vienen de mongo"
    This reverts commit 3cb93e75d956de2c12001d0c652d1ef12bf1573b.

--- eda/eda_api/lib/module/datasource/datasource.controller.ts
+++ eda/eda_api/lib/module/datasource/datasource.controller.ts
@@ -206,16 +206,10 @@ export class DataSourceController {
                 }else{
                     console.log('Importing existing datasource');
                     body.ds.connection.password = psswd === '__-(··)-__' ? dataSource.ds.connection.password : EnCrypterService.encrypt(body.ds.connection.password);
-                    //console.log(body.ds);
-                    let cadena = JSON.stringify(body.ds);
-                    cadena = cadena.split('$oid').join('id');
-                    JSON.parse(cadena.toString());
-                    dataSource.ds =   JSON.parse(cadena.toString());
-                    //dataSource.ds = body.ds;
+                    dataSource.ds = body.ds;
                     ds = dataSource;
-                }       
+                }            
 
-                
                 //aparto las relaciones ocultas para optimizar el modelo.
                 ds.ds.model.tables.forEach(t => {
                     t.no_relations = t.relations.filter(r => r.visible == false)
@@ -223,6 +217,7 @@ export class DataSourceController {
                 ds.ds.model.tables.forEach(t => {
                     t.relations = t.relations.filter(r => r.visible !== false)
                 });
+                
                 /** Comprobacionde la reciprocidad de las relaciones */
                 ds.ds.model.tables.forEach(tabla => {
                 tabla.relations.forEach(relacion=> {
@@ -250,13 +245,16 @@ export class DataSourceController {
                  });
 
 
+
+
                 const iDataSource = new DataSource(ds);
+
+
+                //console.log(dataSource.ds.model.tables);
 
                 iDataSource.save((err, dataSource) => {
                     if (err) {
-                        console.log(err);
                         next(new HttpException(500, 'Error updating dataSource'));
-
                     }
 
                     return res.status(200).json({ ok: true, message: 'Modelo actualizado correctamente' });
