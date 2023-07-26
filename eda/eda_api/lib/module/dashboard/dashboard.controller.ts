985d136da3a6fa4a6fff448f4804ae86b2192cc3
Asaf <asaf@jortilles.com>
2023-07-26 09:21:08 +0200

    SDA #78: añadido usuario desconocido para no bloquear lista

--- eda/eda_api/lib/module/dashboard/dashboard.controller.ts
+++ eda/eda_api/lib/module/dashboard/dashboard.controller.ts
@@ -156,6 +156,15 @@ export class DashboardController {
               { _id: dashboard.user },
               'name'
             ).exec()
+            if (dashboard.user == null) {dashboard.user = new User(
+              {
+                name: 'N/A', 
+                email: '',
+                password: '',
+                img: '',
+                role: '',
+                active: ''
+            })};
             privates.push(dashboard)
             break
           case 'group':
@@ -614,9 +623,7 @@ export class DashboardController {
 
 
     try {
-      const connection = await ManagerConnectionService.getConnection(
-        req.body.model_id
-      )
+      const connection = await ManagerConnectionService.getConnection(req.body.model_id);
       const dataModel = await connection.getDataSource(req.body.model_id)
       /**--------------------------------------------------------------------------------------------------------- */
       /**Security check */
