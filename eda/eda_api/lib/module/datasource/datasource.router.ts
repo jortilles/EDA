38990094dd83fdbf237d6b062957087bb730267d
Asaf <asaf@jortilles.com>
2023-09-15 11:19:16 +0200

    modificado roleguard
    añadido roleguard para dashboard para evitar acceso mediante URL

--- eda/eda_api/lib/module/datasource/datasource.router.ts
+++ eda/eda_api/lib/module/datasource/datasource.router.ts
@@ -6,30 +6,30 @@ import { DashboardController } from '../dashboard/dashboard.controller';
 
 const router = express.Router();
 
-router.get('', authGuard, DataSourceController.GetDataSources);
+router.get('', authGuard, roleGuard,  DataSourceController.GetDataSources);
 
-router.get('/names', authGuard, /* roleGuard,*/ DataSourceController.GetDataSourcesNames);
+router.get('/names', authGuard,   roleGuard,  DataSourceController.GetDataSourcesNames);
 
 router.get('/namesForDashboard', authGuard,  DataSourceController.GetDataSourcesNamesForDashboard)
 
 router.get('/namesForEdit', authGuard,  DataSourceController.GetDataSourcesNamesForEdit)
 
-router.get('/check-connection', authGuard, /* roleGuard,*/ DataSourceController.CheckConnection);
+router.get('/check-connection', authGuard,   roleGuard, DataSourceController.CheckConnection);
 
-router.get('/check-connection/:id', authGuard, /* roleGuard,*/ DataSourceController.CheckStoredConnection);
+router.get('/check-connection/:id', authGuard, roleGuard, DataSourceController.CheckStoredConnection);
 
-router.get('/:id', authGuard, /* roleGuard,*/ DataSourceController.GetDataSourceById);
+router.get('/:id', authGuard,  roleGuard, DataSourceController.GetDataSourceById);
 
-router.post('/add-data-source/', authGuard, /* roleGuard,*/ DataSourceController.GenerateDataModel);
+router.post('/add-data-source/', authGuard,  roleGuard, DataSourceController.GenerateDataModel);
 
-router.post('/query', authGuard, /* roleGuard,*/ DashboardController.execQuery);
+router.post('/query', authGuard,  roleGuard, DashboardController.execQuery);
 
-router.post('/reload/:id', authGuard, /* roleGuard,*/ DataSourceController.RefreshDataModel);
+router.post('/reload/:id', authGuard, roleGuard, DataSourceController.RefreshDataModel);
 
-router.post('/remove-cache', authGuard, /* roleGuard,*/ DataSourceController.removeCacheFromModel);
+router.post('/remove-cache', authGuard,  roleGuard, DataSourceController.removeCacheFromModel);
 
-router.put('/:id', authGuard, /* roleGuard,*/ DataSourceController.UpdateDataSource);
+router.put('/:id', authGuard,  roleGuard,DataSourceController.UpdateDataSource);
 
-router.delete('/:id', authGuard, /* roleGuard,*/ DataSourceController.DeleteDataSource);
+router.delete('/:id', authGuard,  roleGuard, DataSourceController.DeleteDataSource);
 
 export default router;
