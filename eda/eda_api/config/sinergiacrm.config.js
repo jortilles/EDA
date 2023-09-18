c330db76bb210bf742bcef91e1aea858f01e3e84
Jortilles <juanjo@jortilles.com>
2023-09-18 14:02:30 +0200

    Create sinergiacrm.config.js
    Restauración del archivo  sinergiacrm.config.js

---
+++ eda/eda_api/config/sinergiacrm.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    sinergiaConn:{
+        host: "@@host@@",
+        port: @@port@@,
+        database: "@@database@@",
+        user: "@@user@@",
+        password: "@@password@@",
+        connectionLimit: 10 ,
+        waitForConnections: true,
+        queueLimit: 0
+      }
+};
