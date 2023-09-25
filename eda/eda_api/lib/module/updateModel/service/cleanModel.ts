caa0175883b5365237aa32eb3332f2d26da86777
Asaf <asaf@jortilles.com>
2023-09-25 17:34:01 +0200

    arreglo cleanModel

--- eda/eda_api/lib/module/updateModel/service/cleanModel.ts
+++ eda/eda_api/lib/module/updateModel/service/cleanModel.ts
@@ -2,14 +2,12 @@ import _ from "lodash";
 import { Sda_Basic_Group } from "./sda_basic.group";
 import Group, { IGroup } from '../../admin/groups/model/group.model'
 import DataSourceSchema from '../../datasource/model/datasource.model'
-import { match } from "assert";
-
 
 export class CleanModel {
     
     public async cleanModel(main_model : any) : Promise<any> {
 
-        const roles = _.cloneDeep(main_model.ds.metadata.model_granted_roles);
+        let roles = _.cloneDeep(main_model.ds.metadata.model_granted_roles);
         
         let model = {
             "users": [],
@@ -33,10 +31,18 @@ export class CleanModel {
             "type": ""
         }
 
-       
-        let model_granted_roles = [] ;
+        let groupModel =  {
+            "groups": [],
+            "groupsName": [],
+            "none": "",
+            "table": "",
+            "column": "",
+            "global": "",
+            "permission": "",
+            "type": ""
+        }
         
-
+        let model_granted_roles = [] ;
 
         for (let i=0;i<roles.length;i++) {
 
@@ -46,18 +52,28 @@ export class CleanModel {
             
             } else {
                 
-                const match = model_granted_roles.find(r => r.table == roles[i].table && r.column == roles[i].column);
-                
-                if (_.isEmpty(match) == false) {
+                const match = model_granted_roles.find(r => r.table == roles[i].table && r.column == roles[i].column && r.type == roles[i].type );
+
+                if (_.isEmpty(match) == false && roles[i].type == "users") {
                     if (!match.users.includes(roles[i].users[0])) {match.users.push(roles[i].users[0]) } ;
                     if (!match.usersName.includes(roles[i].usersName[0])) {match.usersName.push(roles[i].usersName[0]) } ;
                     modelAc = match;
                     
-                } else {
+                } else if (_.isEmpty(match) == false && roles[i].type == "groups") {
+                    if (!match.groups.includes(roles[i].groups[0])) {match.groups.push(roles[i].groups[0]) } ;
+                    if (!match.groupsName.includes(roles[i].groupsName[0])) {match.groupsName.push(roles[i].groupsName[0]) } ;
+                    groupModel = match;        
+                } 
+                else {
                     if (_.isEmpty(modelAc.table) == false) {model_granted_roles.push(modelAc)};
-                    model = roles[i];
+                    if (_.isEmpty(groupModel.table) == false) {model_granted_roles.push(groupModel)};
+                    if (roles[i].type == "groups") {
+                        groupModel = roles[i];    
+                        if (_.isEmpty(groupModel.table) == false) {model_granted_roles.push(groupModel)} ;
+                    } else {
+                        model = roles[i];
+                    }
                     if (_.isEmpty(model.table) == false) {model_granted_roles.push(model)} ;
-                    
 
                 }
 
