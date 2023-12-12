9a036b2c1d836e76601c5b8f6debdf320e6dc5d3
juanSTIC <juan@sinergiacrm.org>
2023-12-12 18:16:12 +0100

    wip

--- eda/eda_app/src/app/module/pages/about/about.component.ts
+++ eda/eda_app/src/app/module/pages/about/about.component.ts
@@ -1,23 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { sinergiaConn } from "../../../../../../eda_api/config/sinergiacrm.config.js";
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  selector: "app-about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"]
 })
 export class AboutComponent implements OnInit {
-
   // Estos valores son solo ejemplos. Debes reemplazarlos con datos reales, posiblemente obtenidos desde un servicio.
-  sinergiaDaVersion: string = '2.0.1';
-  edaVersion: string = '1.4.0';
-  lastSyncDate: string = '2023-11-16 15:00:00';
-  lastSyncDateScriptVersion: string = '20231116150000';
+  sinergiaDaVersion: string = "2.0.1";
+  edaVersion: string = "1.4.0";
+  lastSyncDate: string = "2023-11-16 15:00:00";
+  lastSyncDateScriptVersion: string = "20231116150000";
+  sinergiaCRMDatabaseName: string = "Undfind";
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    // Aquí puedes cargar los datos reales, por ejemplo, desde un servicio.
-
+    this.sinergiaCRMDatabaseName = sinergiaConn.database;
   }
-
 }
