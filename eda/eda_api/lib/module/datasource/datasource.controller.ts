68be92216ef3f1a1bb4b673171921b5c5af7b811
Juanjo Ortilles <juanjo@jortilles.com>
2024-02-28 15:38:01 +0100

    test

--- eda/eda_api/lib/module/datasource/datasource.controller.ts
+++ eda/eda_api/lib/module/datasource/datasource.controller.ts
@@ -82,6 +82,7 @@ export class DataSourceController {
     Un cop filtrats els permisos de grup i de usuari. */
     static async GetDataSourcesNamesForDashboard(req: Request, res: Response, next: NextFunction) {
         const userID = req.user._id;
+        console.log(userID);
         let options:QueryOptions = {};
         DataSource.find({}, '_id ds.metadata.model_name ds.metadata.model_granted_roles',options, (err, ds) => {
             if (!ds) {
@@ -119,13 +120,18 @@ export class DataSourceController {
                                 });
                         }
                     });
-
+                    console.log(e.ds.metadata.model_owner);
+                    console.log(userID);
+                    console.log('------------------');
                   /**  EDA */ //  if (users.includes(userID) || roles.length > 0 || allCanSee == 'true'  || req.user.role.includes('135792467811111111111110') /* admin role  los admin lo ven todo*/ )  {
                   /**   edalitics free */        if (   e.ds.metadata.model_owner == userID   ||   req.user.role.includes('135792467811111111111110') /* admin role  los admin lo ven todo*/ )  {
                     output.push({ _id: e._id, model_name: e.ds.metadata.model_name });
                     }
 
                 }else {
+                    console.log(e.ds.metadata.model_owner);
+                    console.log(userID);
+                    console.log('------------------');
        /**  // edalitics free */    if (   e.ds.metadata.model_owner ==  userID ||   req.user.role.includes('135792467811111111111110') /* admin role  los admin lo ven todo*/ )  {
                         output.push({ _id: e._id, model_name: e.ds.metadata.model_name });
         /**  // edalitics free */   }
