1f6a5848832cf815ca36aa28ed82d1cd1b1dfb49
Juanjo Ortilles <Juanjo Ortilles@sportium.com>
2024-03-02 05:20:16 +0100

    No Repetitions Version Fianl
    (cherry picked from commit f89f6c8694f7c112f48a44e8a79a6c6c3c000946)
    
    # Conflicts:
    #	eda/eda_app/src/app/module/components/eda-table/eda-table.ts

--- eda/eda_app/src/app/module/components/eda-table/eda-table.ts
+++ eda/eda_app/src/app/module/components/eda-table/eda-table.ts
@@ -9,12 +9,8 @@ import * as _ from 'lodash';
 import { Column } from '@eda/models/model.index';
 import { EdaColumnNumber } from './eda-columns/eda-column-number';
 import { EdaColumnPercentage } from './eda-columns/eda-column-percentage';
-import { Output, EventEmitter, Component } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 import { EdaColumnChart } from './eda-columns/eda-column-chart';
-import { ToastModule } from 'primeng/toast';
-import { Key } from 'protractor';
-import { FindValueSubscriber } from 'rxjs/internal/operators/find';
-import { values } from 'd3';
 
 interface PivotTableSerieParams {
     mainCol: any,
@@ -68,6 +64,8 @@ export class EdaTable {
     public onlyPercentages: boolean = false;
     public percentageColumns: Array<any> = [];
     public noRepetitions: boolean; 
+    public origValues: any[] = [];
+
     public autolayout: boolean = true;
     public sortedSerie: any = null;
     public sortedColumn: any = { field: null, order: null };
@@ -90,6 +88,9 @@ export class EdaTable {
     }
 
     set value(values: any[]) {
+        if( this.origValues.length == 0 ){
+            this.origValues = _.cloneDeep(values);
+        } 
         this.clear();
         this._value = values;
         /* Inicialitzar filtres */
@@ -107,6 +108,7 @@ export class EdaTable {
         if (this.sortedSerie) {
             this.loadSort();
         }
+
 
     }
 
@@ -220,12 +222,9 @@ export class EdaTable {
         if (this.withColSubTotals) {
             event ? this.colSubTotals(event.first / event.rows + 1) : this.colSubTotals(1);
         } 
-        //Aixó no s'executa per les taules creuades
-        
-        if ( !this.pivot) {
-         console.log('desactivadas las no repeticiones');
-        // this.noRepeatedRows();
-         }
+        if (!this.pivot) {
+            this.noRepeatedRows();
+        }
 
     }
 
@@ -531,46 +530,18 @@ export class EdaTable {
     
     noRepeatedRows() {
 
-        //separamos valores de claves
-        let values = this.extractDataValues(this.value);
-                
-        //tomamos claves que serán el cabecero
-        let labels = this.extractLabels(this.value)
-        
-        labels.shift(); //borramos el primer objeto.
-        let first  = _.cloneDeep(values[0]);
-        let output = [];
-
         //esta primera iteración con this.noRepetitions en false se hace para devolver las palabras repetidas al diálogo.
         //Es una secuencia similar a la de quitar los valores, pero opuesta.
         if (!this.noRepetitions) {
-
-            const output = [];
-            let values = this.extractDataValues(this.value);
-            values = values.filter(row => !row.every(element => element === null));
-            // Load the Table for a preview
-            for (let i = 0; i < values.length; i += 1) {
-                const obj = {};
-                if (i == 0) {
-                    for (let e = 0; e < values[i].length; e += 1) {
-                        obj[labels[e]] = values[i][e];       
-                }
-                } else {
-                    for (let e = 0; e < values[i].length; e += 1) {            
-                        if (values[i][e] == "" &&  isNaN(values[i][e])){
-                            obj[labels[e]] = first[e];
-                        } else {
-                            obj[labels[e]] = values[i][e];
-                            first[e] = values[i][e];
-                        }   
-                    }
-                }
-                
-                output.push(obj);
-            }
-            return this.value = output;
-          
+           this.value = this.origValues  ;
+           this.origValues = []; /**libero espacio */
         } else {
+            //separamos valores de claves
+            let values = this.extractDataValues(this.value);
+            //tomamos claves que serán el cabecero
+            let labels = this.extractLabels(this.value)
+            labels.shift(); //borramos el primer objeto.
+            let output = [];
             // ESTO SE HACE PARA EVITAR REPETIDOS EN LA TABLA. SI UN CAMPO TIENE UNA COLUMNA QUE SE REPITE 
             let first  = _.cloneDeep(values[0]);
             for (let i = 0; i < values.length; i += 1) {
@@ -592,10 +563,8 @@ export class EdaTable {
                 output.push(obj);   
             }
             this.value = output;  
-            
         }   
-        return this.value;
-       
+
     }
 
 
--- eda/eda_app/src/app/services/utils/chart-utils.service.ts
+++ eda/eda_app/src/app/services/utils/chart-utils.service.ts
@@ -491,10 +491,7 @@ export class ChartUtilsService {
         return uniqueLabels;
     }
 
-    public transformDataQueryForTable(noRepetitions: boolean, labels: any[], values: any[]) {
-
-        if (noRepetitions !== true) {
-
+    public transformDataQueryForTable( labels: any[], values: any[]) {
             const output = [];
             values = values.filter(row => !row.every(element => element === null));
             // Load the Table for a preview
@@ -506,37 +503,6 @@ export class ChartUtilsService {
                 output.push(obj);
             }
             return output;
-
-        } else {
-
-            const output = [];
-            values = values.filter(row => !row.every(element => element === null));
-            // ESTO SE HACE PARA EVITAR REPETIDOS EN LA TABLA. SI UN CAMPO TIENE UNA COLUMNA QUE SE REPITE
-            let first  = _.cloneDeep(values[0]);
-            for (let i = 0; i < values.length; i += 1) {
-                const obj = [];
-                if(i == 0){
-                    for (let e = 0; e < values[i].length; e += 1) {
-                            obj[labels[e]] = values[i][e];
-                        }
-                }else{
-                    for (let e = 0; e < values[i].length; e += 1) {
-                        if (values[i][e] === first[e]    &&  isNaN(values[i][e]) ) {
-                            obj[labels[e]] = "";   // AQUI SE SUSTITUYEN LOS REPETIDOS POR UNA CADENA EN BLANCO
-                        } else {
-                            obj[labels[e]] = values[i][e];
-                        }
-                        first[e]  =  values[i][e]; //AQUI SE SUTITUYE EL PRIMER VALOR
-                        }
-                }
-                output.push(obj);
-            }
-
-            return output;
-
-        }
-
-
     }
 
     /**
