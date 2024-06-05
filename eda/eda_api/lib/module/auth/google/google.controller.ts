7628c786b954035d899b3a769f5f5cc91f185843
Ronald Chavez <ronald.chavez@jortilles.com>
2024-06-05 15:53:23 +0200

    Instalación de paquete google-auth-library para la validación del credential de Google

--- eda/eda_api/lib/module/auth/google/google.controller.ts
+++ eda/eda_api/lib/module/auth/google/google.controller.ts
@@ -6,6 +6,10 @@ import * as path from 'path';
 import * as fs from 'fs';
 import { QueryOptions } from 'mongoose';
 
+// Importaciones necesarias
+import User, { IUser } from '../../admin/users/model/user.model';
+
+
 
 
 const jwt = require('jsonwebtoken');
@@ -26,21 +30,48 @@ export class GoogleController {
 
 
     static async credentialGoogle(req: Request, res: Response, next: NextFunction) {
-        
-        const respGoogle = req.body.respGoogle;
-        const {credential} = respGoogle;
 
-        console.log('respGoogle: ', respGoogle);
+        try {
+            
+            const body = req.body
+            const {respGoogle} = body;
+            const {credential} = respGoogle;
+            let token: string;
+            let user: IUser = new User({ name: '', email: '', password: '', img: '', role: [] });
+    
+            insertServerLog(req, 'info', 'newLogin', body.email, 'attempt');
+
+            if(credential.length!==0) {
+                console.log('Credential tiene datos')
+                console.log('Verificación del credential con la llave secreta de google')
+            }
+
+            // console.log('respGoogle: ', respGoogle);
+            // console.log('credential: ', credential);
+            // console.log('token: ', token);
+            // console.log('user: ', user);
+    
+    
+            res.json({
+                ok: true,
+                respGoogle: respGoogle
+            })
 
 
-        
-
-
-        res.json({
-            ok: true,
-            respGoogle: respGoogle
-        })
-
+        } catch (error) {
+            next(error);
+        }
     }
+}
+
+function insertServerLog(req: Request, level: string, action: string, userMail: string, type: string) {
+    const ip = req.headers['x-forwarded-for'] || req.get('origin')
+    var date = new Date();
+    var month =date.getMonth()+1 ;
+    var monthstr=month<10?"0"+month.toString(): month.toString();
+    var day = date.getDate();
+    var daystr=day<10?"0"+day.toString(): day.toString();
+    var date_str = date.getFullYear() + "-" + monthstr + "-" + daystr + " " +  date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    ServerLogService.log({ level, action, userMail, ip, type, date_str});
 }
 
