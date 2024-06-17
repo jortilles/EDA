800df3d26c07ef36224d848fe1b46357a27e7534
Juanjo Ortilles <juanjo@jortilles.com>
2024-06-17 16:08:51 +0200

    work in progresss

--- eda/eda_app/src/app/services/guards/login-guard.guard.ts
+++ eda/eda_app/src/app/services/guards/login-guard.guard.ts
@@ -9,6 +9,13 @@ export class LoginGuardGuard implements CanActivate {
         public router: Router) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        console.log( route);
+        if(route.queryParams.token){
+            console.log('aqui tengo el token!!!!!');
+            console.log('llamo a la api y recupero los valores del usuario');
+            // savingStorage( id: string, token: string, user: User) ;
+            // savingStorage( id de usuario 135792467811111111111111, token: el token que me llega, user: el objeto user del mongo) ;
+        }
         if (this.userService.isLogged()) {
             return true;
         } else {
