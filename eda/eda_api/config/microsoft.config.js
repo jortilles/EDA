1a2b65a48665332729f7ffdbba115776a40e5dd3
Ronald Chavez <ronald.chavez@jortilles.com>
2024-06-17 12:02:39 +0200

    microsoft credentials

---
+++ eda/eda_api/config/microsoft.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    CLOUD_INSTANCE: 'xxxxxxxxxxxxxxx',
+    TENANT_ID: 'xxxxxxxxxxxxxxx',
+    CLIENT_ID: 'xxxxxxxxxxxxxxx',
+    CLIENT_SECRET: 'xxxxxxxxxxxxxxx',
+    REDIRECT_URI: 'xxxxxxxxxxxxxxx',
+    POST_LOGOUT_REDIRECT_URI: 'xxxxxxxxxxxxxxx',
+    GRAPH_API_ENDPOINT: 'xxxxxxxxxxxxxxx',
+    EXPRESS_SESSION_SECRET: 'xxxxxxxxxxxxxxx',
+};
