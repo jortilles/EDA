c04ed0786a913115336f6fb35178a9e32e68860b
ronaldchavezjortilles <ronald.chavez@jortilles.com>
2024-06-19 16:03:35 +0200

    Url con queryParams - Ganta

--- eda/eda_app/src/app/services/guards/login-guard.guard.ts
+++ eda/eda_app/src/app/services/guards/login-guard.guard.ts
@@ -11,18 +11,27 @@ export class LoginGuardGuard implements CanActivate {
         public router: Router) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+
         const token = route.queryParams.token; 
         
-        if(token){
-            this.userService.tokenUrl(token).subscribe(() => {
-                this.router.navigate([this.route.snapshot.queryParams['returnUrl'] || '/home']);
-            })
-        }
         if (this.userService.isLogged()) {
             return true;
         } else {
-            this.router.navigate(['/login'], { queryParams: { returnUrl: state.url.split('?')[0], params: state.url.split('?')[1] } });
-            return false;
+
+            if(token){
+                
+                this.userService.tokenUrl(token).subscribe(() => {
+                    const urlInforme = state.url.split('?')[0];
+                    this.router.navigate([urlInforme], { queryParams: route.queryParams });
+                })
+                
+                return false;
+            } else {
+                console.log('aqui Login')
+                this.router.navigate(['/login'], { queryParams: { returnUrl: state.url.split('?')[0], params: state.url.split('?')[1] } });
+                return false;
+            }
+
         }
     }
 }
