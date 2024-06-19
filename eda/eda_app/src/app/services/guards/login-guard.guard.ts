bd2c790aacf40a7cb2c56834d40d27f7235f39f0
Juanjo Ortilles <juanjo@jortilles.com>
2024-06-19 10:00:44 +0200

    work in progress

--- eda/eda_app/src/app/services/guards/login-guard.guard.ts
+++ eda/eda_app/src/app/services/guards/login-guard.guard.ts
@@ -12,17 +12,25 @@ export class LoginGuardGuard implements CanActivate {
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
         const token = route.queryParams.token; 
+        console.log(route);
+        console.log(state);
         
         if(token){
             this.userService.tokenUrl(token).subscribe(() => {
-                this.router.navigate([this.route.snapshot.queryParams['returnUrl'] || '/home']);
+                this.router.navigate( [state.url.split('?')[0] ],
+                                     { queryParams: { returnUrl: state.url.split('?')[0], params: state.url.split('?')[1] } });
+                return true;
             })
+        }else{
+            if (this.userService.isLogged()) {
+                return true;
+            } else {
+                this.router.navigate(['/login'], { queryParams: { returnUrl: state.url.split('?')[0], params: state.url.split('?')[1] } });
+                return false;
+            }
         }
-        if (this.userService.isLogged()) {
-            return true;
-        } else {
-            this.router.navigate(['/login'], { queryParams: { returnUrl: state.url.split('?')[0], params: state.url.split('?')[1] } });
-            return false;
-        }
+
+       
+         
     }
 }
