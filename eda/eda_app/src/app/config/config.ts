a979da399d9f6a5fab6e0280d50e309bf431fecb
ronaldchavezjortilles <ronald.chavez@jortilles.com>
2024-07-26 16:50:13 +0200

    Se agrega al modelo de datos -> Autorelation y bridge

--- eda/eda_app/src/app/config/config.ts
+++ eda/eda_app/src/app/config/config.ts
@@ -1,3 +1,6 @@
-export const URL_SERVICES = '/edapi';
+export const URL_SERVICES = 'http://localhost:8666';
 export const MAX_TABLE_ROWS_FOR_ALERT = 50000;
-
+export const GOOGLE_CLIENT_ID = '134293370744-l1o2qu9g9kqipse32s1r5rq97ichacai.apps.googleusercontent.com'
+export const MICROSOFT_ID = '10bf7839-de46-41f1-95fe-48bbf96505a0'
+export const MICROSOFT_AUTHORITY = 'https://login.microsoftonline.com/common'
+export const MICROSOFT_REDIRECT_URI = 'http://localhost:4200'
