81e8612236df778b037e4f9da42ba18e7f02701b
Juanjo Ortilles <juanjo@jortilles.com>
2024-08-19 12:29:31 +0200

    commit

--- eda/eda_app/src/app/config/config.ts
+++ eda/eda_app/src/app/config/config.ts
@@ -1,7 +1,9 @@
 export const URL_SERVICES = '/edapi';
 export const MAX_TABLE_ROWS_FOR_ALERT = 50000;
-export const GOOGLE_CLIENT_ID = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-export const MICROSOFT_ID = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-export const MICROSOFT_AUTHORITY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-export const MICROSOFT_REDIRECT_URI = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+export const GOOGLE_CLIENT_ID = 'xxxxxxxxxxxxxx'
+export const MICROSOFT_ID = 'xxxxxxxxxxxxxx'
+export const MICROSOFT_AUTHORITY = 'xxxxxxxxxxxxxx'
+export const MICROSOFT_REDIRECT_URI = 'xxxxxxxxxxxxxx'
+
 
