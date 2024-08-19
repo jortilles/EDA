7afbb56cb1ff187965566b2f23e8ae8f4358faed
ronaldchavezjortilles <ronald.chavez@jortilles.com>
2024-08-19 12:11:33 +0200

    config del frontend

--- eda/eda_app/src/app/config/config.ts
+++ eda/eda_app/src/app/config/config.ts
@@ -1,2 +1,8 @@
-export const URL_SERVICES = '/edapi';
-export const MAX_TABLE_ROWS_FOR_ALERT = 50000;
+export const URL_SERVICES = 'xxxxxxxxxxxxxx';
+export const MAX_TABLE_ROWS_FOR_ALERT = 'xxxxxxxxxxxxxx';
+export const GOOGLE_CLIENT_ID = 'xxxxxxxxxxxxxx'
+export const MICROSOFT_ID = 'xxxxxxxxxxxxxx'
+export const MICROSOFT_AUTHORITY = 'xxxxxxxxxxxxxx'
+export const MICROSOFT_REDIRECT_URI = 'xxxxxxxxxxxxxx'
+
+
