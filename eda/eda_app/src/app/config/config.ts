63dd5364718e84fc4b96860abc3d84f4347d4570
Juanjo Ortilles <juanjo@jortilles.com>
2024-11-08 06:13:44 +0100

    edapi

--- eda/eda_app/src/app/config/config.ts
+++ eda/eda_app/src/app/config/config.ts
@@ -1,8 +1,6 @@
-export const URL_SERVICES = 'xxxxxxxxxxxxxx';
-export const MAX_TABLE_ROWS_FOR_ALERT = 'xxxxxxxxxxxxxx';
-export const GOOGLE_CLIENT_ID = 'xxxxxxxxxxxxxx'
-export const MICROSOFT_ID = 'xxxxxxxxxxxxxx'
-export const MICROSOFT_AUTHORITY = 'xxxxxxxxxxxxxx'
-export const MICROSOFT_REDIRECT_URI = 'xxxxxxxxxxxxxx'
-
-
+export const URL_SERVICES = '/edapi';
+export const MAX_TABLE_ROWS_FOR_ALERT = 50000;
+export const GOOGLE_CLIENT_ID = 'xxxxxxxxxxxxxx';
+export const MICROSOFT_ID = 'xxxxxxxxxxxxxx';
+export const MICROSOFT_AUTHORITY = 'xxxxxxxxxxxxxx';
+export const MICROSOFT_REDIRECT_URI = 'xxxxxxxxxxxxxx';
