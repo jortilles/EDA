e6c0c5ed739edd8e87785f527e174b1eda55bf3b
ronaldchavezjortilles <ronald.chavez@jortilles.com>
2025-03-13 15:21:08 +0100

    Ordenamiento de componente

--- eda/eda_app/src/app/module/components/eda-filter-and-or/eda-filter-and-or.component.ts
+++ eda/eda_app/src/app/module/components/eda-filter-and-or/eda-filter-and-or.component.ts
@@ -24,18 +24,15 @@ import {
   encapsulation: ViewEncapsulation.None
 })
 export class EdaFilterAndOrComponent implements OnInit {
+  
+  @Input() selectedFilters: any[] = []; // Valor que es 
+  @Input() globalFilters: any[] = []; // Valor que es 
 
   options: GridsterConfig;
   dashboard: GridsterItem[];
   itemToPush!: GridsterItemComponent;
   selectedButton: any[]; 
   selectedButtonInitialValue: string; // Valor seleccionado por defecto
-
-
-  public height: number = 0;
-
-  @Input() selectedFilters: any[] = []; // Valor que es 
-  @Input() globalFilters: any[] = []; // Valor que es 
 
   constructor() { 
     this.options = {
@@ -60,7 +57,6 @@ export class EdaFilterAndOrComponent implements OnInit {
       // disableScrollHorizontal: true, // Desactiva scroll horizontal si es necesario
       // disableScrollVertical: true, // Desactiva scroll horizontal si es necesario
       itemChangeCallback: (item: GridsterItem) => this.onItemChange(item),
-      itemResizeCallback: (item: GridsterItem) => this.onItemChange(item),
     };
 
     // Valores and y or
@@ -72,8 +68,6 @@ export class EdaFilterAndOrComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    // console.log('Global Filter: ', this.globalFilters);
-    // console.log('Panel Filter: ', this.selectedFilters);
 
     // Integración:
     this.dashboard = [];
@@ -88,84 +82,17 @@ export class EdaFilterAndOrComponent implements OnInit {
       this.dashboard.push({cols: 3, rows:1, y: this.globalFilters.length + j, x:0, filter_column: sf.filter_column, value: "and"})
     })
 
-    // this.dashboard = [
-    //   { cols: 3, rows: 1, y: 0, x: 0, name: 'Edalitics 1' },
-    //   { cols: 3, rows: 1, y: 1, x: 0, name: 'Edalitics 2' },
-    //   { cols: 3, rows: 1, y: 2, x: 0, name: 'Edalitics 3'},
-    //   { cols: 3, rows: 1, y: 3, x: 0, name: 'Edalitics 4'},
-    //   { cols: 3, rows: 1, y: 4, x: 0, name: 'Edalitics 5'},
-    // ];
-
-  }
-
-  
-  changedOptions(): void {
-    if (this.options.api && this.options.api.optionsChanged) {
-      this.options.api.optionsChanged();
-    }
-  }
-
-  removeItem($event: MouseEvent | TouchEvent, item: any): void {
-    $event.preventDefault();
-    $event.stopPropagation();
-    this.dashboard.splice(this.dashboard.indexOf(item), 1);
-    this.onItemChange(item);
-  }
-
-  addItem(): void {
-    this.dashboard.push({ x: 0, y: 0, cols: 1, rows: 1 });
-  }
-
-  initItem(item: GridsterItem, itemComponent: any): void {
-    this.itemToPush = itemComponent;
-  }
-
-  pushItem(): void {
-    const push = new GridsterPush(this.itemToPush); // init the service
-    this.itemToPush.$item.rows += 4; // move/resize your item
-    if (push.pushItems(push.fromNorth)) {
-      // push items from a direction
-      push.checkPushBack(); // check for items can restore to original position
-      push.setPushedItems(); // save the items pushed
-      this.itemToPush.setSize();
-      this.itemToPush.checkItemChanges(
-        this.itemToPush.$item,
-        this.itemToPush.item
-      );
-    } else {
-      this.itemToPush.$item.rows -= 4;
-      push.restoreItems(); // restore to initial state the pushed items
-    }
-    push.destroy(); // destroy push instance
-    // similar for GridsterPushResize and GridsterSwap
-  }
-
-  getItemComponent(): void {
-    if (this.options.api && this.options.api.getItemComponent) {
-      console.log('this.dashboard: ',this.dashboard)
-      console.log(this.options.api.getItemComponent(this.dashboard[0]));
-    }
   }
 
   onItemChange(item: GridsterItem): void {
-    console.log('Cambio en el ítem:', item);
-    console.log('Todos los valores:', this.dashboard);
-
+    
     const topMostItem = this.getTopMostItem();
     const bottomMostItem = this.getBottomMostItem()
-    let altura = (bottomMostItem!.y + bottomMostItem!.cols + 3) * 250;
-    if(altura<5){
-      this.height = 4*250;
-    } else {
-      this.height = altura;
-    }
-
-    // this.height = 4 * 250
-
-    // this.getBottomMostItem(); // Actualizamos el ítem más bajo cada vez que algo cambia
+    
+    console.log('Cambio en el ítem:', item);
+    console.log('Todos los valores:', this.dashboard);
     console.log('Ítem más alto:', topMostItem);
     console.log('Ítem más bajo:', bottomMostItem);
-
   }
 
     // Función para detectar el ítem más bajo
