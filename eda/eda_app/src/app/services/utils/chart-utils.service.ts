cffabca2548e4328b665df10f2e91ca0869f5d7b
MarcCornejoJortilles <marc.cornejo@jortilles.com>
2025-03-20 11:09:54 +0100

    filtro internacionalizado

--- eda/eda_app/src/app/services/utils/chart-utils.service.ts
+++ eda/eda_app/src/app/services/utils/chart-utils.service.ts
@@ -618,7 +618,7 @@ export class ChartUtilsService {
                     // Si no es string, y es un objeto
                     if (typeof values[i][e] === 'object') {
                         // Si el anterior valor era más duplicado
-                        const isMesDuplicat = values[i][e - 1] === "Mes Duplicat";
+                        const isMesDuplicat = ['Mes repetit', 'Más repetido', 'Most repeated'].includes(values[i][e - 1]);
                         //Contamos todos los valores de la array, y dependiendo de isMesDuplicat volvemos la array inversa o no
                         const sortedEntries = Object.entries(values[i][e].data.reduce((count, valor) => {
                             count[valor] = (count[valor] || 0) + 1;
@@ -627,7 +627,6 @@ export class ChartUtilsService {
 
                         values[i][e] = sortedEntries[0][0];
                     }
-
                     obj[labels[e]] = values[i][e];
                 }
                 output.push(obj);
