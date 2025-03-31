54d697b0d781f1075ce6bed5233204d8a8f61157
Juanjo Ortilles <juanjo@jortilles.com>
2025-03-31 07:01:56 +0200

    update

--- eda/eda_api/config/eda_api_config.js
+++ eda/eda_api/config/eda_api_config.js
@@ -1,4 +1,6 @@
 module.exports = {
-    //podem modificar el valor null de la bbdd per a que ens otorgui un altre valor de lectura en pantalla
-    null_value: ''
-  }
+  //podem modificar el valor null de la bbdd per a que ens otorgui un altre valor de lectura en pantalla
+  null_value: '',
+  log_file: "/root/.pm2/logs/server-out.log", // log de consoltas del servidor
+  error_log_file: "/root/.pm2/logs/server-error.log" // log de errores del servidor
+}
