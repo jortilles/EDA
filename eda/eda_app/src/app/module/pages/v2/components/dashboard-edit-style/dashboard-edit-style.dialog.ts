878b823b86ba3873b7c46c3db5754a60d1bfa557
MarcCornejoJortilles <marc.cornejo@jortilles.com>
2025-05-05 10:33:47 +0200

    editstyle v1.0.0

--- eda/eda_app/src/app/module/pages/v2/components/dashboard-edit-style/dashboard-edit-style.dialog.ts
+++ eda/eda_app/src/app/module/pages/v2/components/dashboard-edit-style/dashboard-edit-style.dialog.ts
@@ -3,14 +3,24 @@ import { FormsModule, ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup,
 import { AlertService, GroupService, IGroup } from "@eda/services/service.index";
 import { EdaDialog, EdaDialog2Component, EdaDialogAbstract, EdaDialogCloseEvent } from "@eda/shared/components/shared-components.index";
 import { SharedModule } from "@eda/shared/shared.module";
+import { DashboardStyles, StyleProviderService } from "@eda/services/service.index";
 import { SelectItem } from "primeng/api";
 import { MultiSelectModule } from "primeng/multiselect";
 import { SelectButtonModule } from "primeng/selectbutton";
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { SliderModule } from 'primeng/slider';
+import { ColorPickerModule } from 'primeng/colorpicker';
+import { RadioButtonModule } from 'primeng/radiobutton';
+
+
+import { DEFAULT_BACKGROUND_COLOR, DEFAULT_PANEL_COLOR, DEFAULT_FONT_FAMILY } from "@eda/configs/personalitzacio/customizables";
+
 @Component({
   selector: 'app-dashboard-edit-style',
   standalone: true,
   templateUrl: './dashboard-edit-style.dialog.html',
-  imports: [SharedModule, ReactiveFormsModule, FormsModule, SelectButtonModule, MultiSelectModule]
+  imports: [SharedModule, ReactiveFormsModule, FormsModule, SelectButtonModule,
+    MultiSelectModule, FloatLabelModule, SliderModule, ColorPickerModule,RadioButtonModule]
 })
 export class DashboardEditStyleDialog {
 @Output() close: EventEmitter<any> = new EventEmitter<any>();
@@ -19,26 +29,183 @@ export class DashboardEditStyleDialog {
   public visibleTypes: SelectItem[] = [];
 
   public display: boolean = false;
+  public backgroundColor: string = DEFAULT_BACKGROUND_COLOR;
+  public panelColor: string = DEFAULT_PANEL_COLOR;
 
-  constructor(
-    private formBuilder: UntypedFormBuilder,
-    private alertService: AlertService) { }
+  
+  public fonts: Array<any> =
+  [
+    { label: 'Montserrat', value: 'Montserrat' },
+    { label: 'Questrial', value: 'Questrial' },
+    { label: 'League Spartan', value: 'League Spartan' },
+    { label: 'Raleway', value: 'Raleway' },
+    { label: 'Bangers', value: 'Bangers' },
+    { label: 'Serif', value: 'Serif' },
+    { label: 'Sans-serif', value: 'Sans-serif' },
+    { label: 'Monospace', value: 'Monospace' },
+    { label: 'Cursive', value: 'Cursive' },
+    { label: 'Papyrus', value: 'Papyrus' },
+    { label: 'Courier New', value: 'Courier New' }
+    
+  ].sort((a, b) => a.label.localeCompare(b.label));
 
+  public dashBoardStyles: DashboardStyles;
+
+  public titleHeader: string = $localize`:@@dashboardTitleEdit:Título del informe`;
+  public filtersHeader: string = $localize`:@@filtersEdit:Filtros`;
+  public panelTitleHeader: string = $localize`:@@panelTitleEdit:Título del panel`;
+  public panelContentHeader: string = $localize`:@@panelContentEdit:Contenido del panel`;
+  public samplePanelTitle: string = $localize`:@@samplePanelTitle:Previsualización`;
+  public samplePanelDBName: string = $localize`:@@samplePanelName:Título del informe`;
+  public samplePanelName: string = $localize`:@@samplePanelName:Título del panel`;
+  public filtrar: string = $localize`:@@filterButtonDashboard:Filtrar`;
+  public css: string;
+
+  public left: string = $localize`:@@left:Izquierda`;
+  public center: string = $localize`:@@center:Centro`;
+  public right: string = $localize`:@@right:Derecha`;
+
+  /**Dashboard title */
+  public selectedTitleFont: any;
+  public titleFontSize: number = this.stylesProviderService.DEFAULT_FONT_SIZE;
+  public titleFontColor: string = this.stylesProviderService.DEFAULT_FONT_COLOR;
+  public alignDasboardTitle: string = this.stylesProviderService.DEFAULT_TITLE_ALIGN;
+
+  /**Filters */
+  public selectedFiltersFont: any;
+  public filtersFontSize: number = this.stylesProviderService.DEFAULT_FONT_SIZE;
+  public filtersFontColor: string = this.stylesProviderService.DEFAULT_FONT_COLOR;
+
+  /**Panel title */
+  public selectedPanelTitleFont: any;
+  public panelTitleFontSize: number = this.stylesProviderService.DEFAULT_FONT_SIZE;
+  public panelTitleFontColor: string = this.stylesProviderService.DEFAULT_FONT_COLOR;
+  public alignPanelTitle: string = this.stylesProviderService.DEFAULT_PANEL_TITLE_ALIGN;
+
+  /**Panel content */
+  public selectedPanelFont: any;
+  public panelFontSize: number = this.stylesProviderService.DEFAULT_FONT_SIZE;
+  public panelFontColor: string = this.stylesProviderService.DEFAULT_FONT_COLOR;
+
+
+
+  public sampleTextFont: string = DEFAULT_FONT_FAMILY;
+  public sampleTextStle: {} = {};
+
+  /**Sample panel */
+  public sampleGlobalStyle: {};
+  public sampleTitleStyle: {};
+  public sampleFiltersStyle: {};
+  public samplePanelTitleStyle: {};
+  public samplePanelContentStyle: {};
+  public panelBG: string;
+
+    
+    
+  constructor(private formBuilder: UntypedFormBuilder, private alertService: AlertService
+    , private stylesProviderService: StyleProviderService) {
+      this.dashBoardStyles = {} as DashboardStyles;
+      this.dialog = new EdaDialog({
+        show: () => this.onShow(),
+        hide: () => this.onClose(),
+        title: $localize`:@@editStylesTitle:EDITAR ESTILOS DEL INFORME`,
+      });
+  }
   ngOnInit(): void {
-    this.initializeForm();
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    console.log(this)
   }
 
+  onShow(): void {
+    //this.dashBoardStyles = this.controller.params;
+    console.log(this)
+		this.assignValues(this.dashBoardStyles);
+		this.setSampleTitleStyle();
+		this.setSampleFIltersStyle();
+		this.setSampleGlobalStyle();
+		this.setPanelTitleStyle();
+		this.setPanelContentStyle();
+	}
 
-  private initializeForm(): void {
-    this.form = this.formBuilder.group({
-      name: [null, Validators.required],
-      visible: [null, Validators.required],
-      group: [null]
-    });
+  assignValues(styles: DashboardStyles): void {
 
-    this.form.controls['visible'].setValue(this.visibleTypes[2].value);
-  }
+		/**Title */
+		this.selectedTitleFont = { label: styles.title.fontFamily, value: styles.title.fontFamily }
+		this.titleFontColor = styles.title.fontColor;
+		this.titleFontSize = styles.title.fontSize;
+		this.alignDasboardTitle = styles.titleAlign;
 
+		/**Filters */
+		this.selectedFiltersFont = { label: styles.filters.fontFamily, value: styles.filters.fontFamily }
+		this.filtersFontColor = styles.filters.fontColor;
+		this.filtersFontSize = styles.filters.fontSize;
+
+		/**Panel title */
+		this.selectedPanelTitleFont = { label: styles.panelTitle.fontFamily, value: styles.panelTitle.fontFamily }
+		this.panelTitleFontColor = styles.panelTitle.fontColor;
+		this.panelTitleFontSize = styles.panelTitle.fontSize;
+		this.alignPanelTitle = styles.panelTitleAlign;
+
+		/**Panel content */
+		this.selectedPanelFont = { label: styles.panelContent.fontFamily, value: styles.panelContent.fontFamily }
+		this.panelFontColor = styles.panelContent.fontColor;
+		this.panelFontSize = styles.panelContent.fontSize;
+
+		this.css = styles.customCss;
+
+	}
+
+	public setSampleGlobalStyle() {
+
+		this.sampleGlobalStyle = {
+			'background-color': this.dashBoardStyles.backgroundColor,
+		}
+	}
+
+	public setSampleTitleStyle() {
+		this.sampleTitleStyle = {
+			'background-color': this.dashBoardStyles.panelColor,
+			'font-family': this.selectedTitleFont.value,
+			'color': this.titleFontColor,
+			'text-align': this.alignDasboardTitle === 'flex-start' ? 'left' : this.alignDasboardTitle === 'center' ? 'center' : 'right',
+			'font-size': `${this.titleFontSize / 10 + 1.8}rem`,
+			'margin-bottom': 'auto',
+			'font-weight': '600',
+			'white-space': 'nowrap',
+			'padding': '0.1em',
+			'box-shadow': '0 2px 1px -1px rgb(0 0 0 / 20%), 0 1px 1px 0 rgb(0 0 0 / 14%), 0 1px 3px 0 rgb(0 0 0 / 12%)'
+		}
+	}
+
+	public setSampleFIltersStyle() {
+		this.sampleFiltersStyle = {
+			'font-family': this.selectedFiltersFont.value,
+			'color': this.filtersFontColor,
+			'font-size': `${this.filtersFontSize / 10 + 1.1}rem`,
+		}
+	}
+
+	public setPanelTitleStyle() {
+		this.samplePanelTitleStyle = {
+			'font-family': this.selectedPanelTitleFont.value,
+			'color': this.panelTitleFontColor,
+			'text-align': this.alignPanelTitle,
+			'font-size': `${this.panelTitleFontSize / 10 + 1.2}rem`,
+		}
+	}
+
+	public setPanelContentStyle() {
+		this.samplePanelContentStyle = {
+			'font-family': this.selectedPanelFont.value,
+			'color': this.panelFontColor,
+			'font-size': `${this.panelFontSize / 10 + 1}rem`,
+		};
+	}
+
+	public setCustomStyle() {
+
+	}
 
   public onApply() {
     this.display = false;
