f7519cd8e92772807070483a8d718e3300668cb8
MarcCornejoJortilles <marc.cornejo@jortilles.com>
2025-08-14 10:38:52 +0200

    backgroundPage tamaño entero

--- eda/eda_app/src/app/module/pages/v2/pages-v2.ts
+++ eda/eda_app/src/app/module/pages/v2/pages-v2.ts
@@ -5,6 +5,7 @@ import { MainLeftSidebarComponent } from "./components/main-left-sidebar/main-le
 import { CreateDashboardService } from "@eda/services/utils/create-dashboard.service";
 import { CreateDashboardComponent } from "@eda/shared/components/create-dashboard/create-dashboard.component";
 import { StyleProviderService} from '@eda/services/service.index';
+import { Subscription } from "rxjs";
 
 @Component({
     selector: 'app-v2-pages',
@@ -15,25 +16,34 @@ import { StyleProviderService} from '@eda/services/service.index';
 export class PagesV2Component implements OnInit {
     private router = inject(Router);
     public createDashboardService = inject(CreateDashboardService);
+    public styleProviderService = inject(StyleProviderService);
     backgroundColor: string
     panelMode: boolean = false;
+    bgColor: string = '';
+    private bgSub?: Subscription;
     // private route: ActivatedRoute,
     // private settingSerive: SettingsService,
     // private styleProviderService: StyleProviderService
-    constructor(private styleProviderService: StyleProviderService) {
+    constructor() {
 
         // this.styleProviderService.pageBackground.subscribe((backgroundColor) => {
         //     this.backgroundColor = backgroundColor;
         // })
 
     }
-    bgClass: any = this.styleProviderService.pageBackground;
 
 
     ngOnInit(): void {
         // this.settingSerive.loadingSettings();
         // this.getPanelMode();
         // init_plugins();
+        this.bgSub = this.styleProviderService.pageBackground.subscribe(value => {
+          this.bgColor = value;
+        });
+    }
+
+    ngOnDestroy() {
+        this.bgSub?.unsubscribe();
     }
 
     onCloseCreateDashboard(event: any) {
