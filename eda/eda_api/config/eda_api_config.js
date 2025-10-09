0175ab86db99dbe2ca1d01687b05fa66fd087b98
ronaldchavezjortilles <ronald.chavez@jortilles.com>
2025-10-09 16:45:54 +0200

    Primera prueba de conexión SAML

--- eda/eda_api/config/eda_api_config.js
+++ eda/eda_api/config/eda_api_config.js
@@ -2,12 +2,12 @@ module.exports = {
     //podem modificar el valor null de la bbdd per a que ens otorgui un altre valor de lectura en pantalla
     null_value: '',
     authentication_type: {
-      type: 'sso_mixto',
-      native: true,
+      type: 'sso',
+      native: "true",
       options: {
-        authentication: "saml",
-        authorization: "bbdd_orcl",
-        elements: []
+        authentication: "sso",
+        authorization: "sso",
+        elements: ["saml", "oauth", "google", "microsoft"]
       }
     },
     log_file: "XXXXXXXXXXXXXXXXXX", // log de consoltas del servidor
