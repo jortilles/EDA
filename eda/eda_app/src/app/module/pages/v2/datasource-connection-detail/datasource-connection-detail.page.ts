48aee6c052ee68c804353ac9c01a107b60e0e5d8
MarcCornejoJort <marc.cornejo@jortilles.com>
2025-12-02 17:29:55 +0100

    falla values lo demas va

--- eda/eda_app/src/app/module/pages/v2/datasource-connection-detail/datasource-connection-detail.page.ts
+++ eda/eda_app/src/app/module/pages/v2/datasource-connection-detail/datasource-connection-detail.page.ts
@@ -2,12 +2,14 @@ import { Component, ElementRef, inject, OnInit, signal, ViewChild } from '@angul
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataSourceService, SpinnerService, AlertService, StyleProviderService, ExcelFormatterService, CsvFormatterService, UploadFileService } from '@eda/services/service.index';
-import { ConfirmationService, SharedModule } from 'primeng/api';
+import { ConfirmationService, SharedModule, SelectItem } from 'primeng/api';
 import Swal from 'sweetalert2';
 import { CommonModule } from '@angular/common';
 import { UploadFileComponent } from '../../data-sources/data-source-detail/upload-file/upload-file.component';
 import { IconComponent } from '@eda/shared/components/icon/icon.component';
 import { lastValueFrom } from 'rxjs';
+import { NgxCsvParser } from 'ngx-csv-parser';
+
 
 @Component({
   standalone: true,
@@ -22,11 +24,15 @@ export class DataSourceConnectionDetailPage implements OnInit {
   @ViewChild('fileUploader', { static: false }) fileUploader: UploadFileComponent;
   @ViewChild('excelFile', { static: false }) excelFile: ElementRef<HTMLInputElement>;
 
-  public header: string = $localize`:@@DataModelHeader:Configurar nueva fuente de datos`;
+  public header: string = $localize`:@@DataModelHeader:Configurar nueva fuente de datos`;  
+  public header2 = true;
   public optimizeString: string = $localize`:@@optimizedQueries:Optimizar consultas`;
   public allowCacheSTR: string = $localize`:@@enableCache: Habilitar caché`;
   public filterTooltip: string = $localize`:@@filterTooltip:Puedes añadir palabras separadas por comas, que se aplicarán como filtros de tipo LIKE a la hora de recuperar las tablas de tu base de datos`;
   public allowSSLSTR: string = $localize`:@@allowSSL:Conexión mediante SSL`;
+
+  public title = $localize`:@@addTableTitle:Añadir Tabla`;
+  @ViewChild('file', { static: false }) file: { nativeElement: { files: { [key: string]: File; }; value: string; click: () => void; }; };
 
   public excelFileName: string = "";
   public csvFileName: string = "";
@@ -53,7 +59,7 @@ export class DataSourceConnectionDetailPage implements OnInit {
     { label: 'jsonWebService', value: 'jsonwebservice' },
     { label: 'Mongo', value: 'mongo', port: 27017 },
     { label: 'Excel', value: 'excel', port: 27017 },
-    // { label: 'Csv', value: 'csv', port: 27017 },
+    { label: 'Csv', value: 'csv', port: 27017 },
   ];
 
   public sidOptions: any[] = [
@@ -73,6 +79,20 @@ export class DataSourceConnectionDetailPage implements OnInit {
   isDraggingExcelFile = signal<boolean>(false);
   isDraggingCsvFile = signal<boolean>(false);
 
+  // variables añadidas ppor el script add-ccsv
+  public csvRecords: any;
+  public csvHeaders: any;
+  public csvColumns: any = [];
+  public delimiter: string;
+  public decDelimiter: string;
+  public tableName: string;
+  public dataTypes: SelectItem[];
+  public dataFormats: SelectItem[];
+  public decSeparators: SelectItem[];
+  public editFieldsHeaders: Array<string>;
+  public names: Array<string>;
+  public tableAdded: boolean = false;
+
   constructor(
     private router: Router,
     private dataSourceService: DataSourceService,
@@ -81,8 +101,43 @@ export class DataSourceConnectionDetailPage implements OnInit {
     public styleProviderService: StyleProviderService,
     private excelFormatterService: ExcelFormatterService,
     private csvFormatterService: CsvFormatterService,
+    private ngxCsvParser: NgxCsvParser,
     private fb: FormBuilder
-  ) {}
+  ) {
+
+
+    this.names = ['type', 'format', 'separator'];
+
+    this.dataTypes = [
+      { label: 'text', value: 'text' },
+      { label: 'integer', value: "integer" },
+      { label: 'numeric', value: "numeric" },
+      { label: 'boolean', value: "boolean" },
+      { label: 'date', value: "timestamp" },
+    ];
+
+    this.editFieldsHeaders = [$localize`:@@csvField:Campo`, $localize`:@@type:Tipo`, $localize`:@@csvFormat:Formato`, $localize`:@@csvSep:Separador Decimal`];
+
+    this.dataFormats = [
+      { label: '', value: '' },
+      { label: '#,###.##', value: '.' },
+      { label: '#.###,##', value: ',' },
+      { label: 'DD-MM-YYYY', value: 'DD-MM-YYYY' },
+      { label: 'YYYY-MM-DD', value: 'YYYY-MM-DD' },
+      { label: 'YYYY-MM-DD HH:MI:SS', value: 'YYYY-MM-DD HH:MI:SS' },
+      { label: 'DD-MM-YYYY HH:MI:SS', value: 'DD-MM-YYYY HH:MI:SS' },
+      { label: 'DD/MM/YYYY', value: 'DD/MM/YYYY' },
+      { label: 'YYYY/MM/DD', value: 'YYYY/MM/DD' },
+      { label: 'DD/MM/YYYY HH:MI:SS', value: 'DD/MM/YYYY HH:MI:SS' },
+      { label: 'YYYY/MM/DD HH:MI:SS', value: 'YYYY/MM/DD HH:MI:SS' },
+    ];
+
+    this.decSeparators = [
+      { label: ',', value: ',' },
+      { label: '.', value: "." }
+    ];
+
+  }
 
   ngOnInit(): void {
     this.styleProviderService.setDefaultBackgroundColor();
@@ -92,6 +147,7 @@ export class DataSourceConnectionDetailPage implements OnInit {
   initForm(): void {
     this.connectionForm = this.fb.group({
       name: ["", Validators.required],
+      separator: ["", Validators.required],
       type: ["postgres", Validators.required],
       host: ["", Validators.required],
       database: ["", Validators.required],
@@ -186,7 +242,7 @@ export class DataSourceConnectionDetailPage implements OnInit {
 
   public async saveCsvDataSource(): Promise<void> {
     const value = this.connectionForm.value;
-
+    console.log(this)
     if (!value.name) {
       this.alertService.addError("No name provided");
     } else {
@@ -272,6 +328,8 @@ export class DataSourceConnectionDetailPage implements OnInit {
 
     const value = this.connectionForm.value;
 
+    console.log(this.excelFileData)
+
     if (!value.name) {
       this.alertService.addError("No name provided");
     } else if (Object.keys(this.excelFileData).length > 0) {
@@ -298,21 +356,23 @@ export class DataSourceConnectionDetailPage implements OnInit {
 
   public async saveCsvJSONCollection(): Promise<void> {
 
+    console.log(this)
     this.spinnerService.on();
-
+    
     const value = this.connectionForm.value;
-
+    console.log(value)
     if (!value.name) {
       this.alertService.addError("No name provided");
-    } else if (Object.keys(this.csvFileData).length > 0) {
+    } else if (Object.keys(this.csvRecords).length > 0) {
       try {
         const fileData = {
           name: value.name,
-          fields: this.csvFileData,
+          fields: this.csvRecords,
           optimize: value.optimize,
           allowCache: value.allowCache
         };
         
+        console.log('voy a hacer el res')
         const res = await lastValueFrom(this.csvFormatterService.addNewCollectionFromJSON(fileData));
 
         this.spinnerService.off();
@@ -367,7 +427,7 @@ export class DataSourceConnectionDetailPage implements OnInit {
       try {
         const jsonData = await this.csvFormatterService.readCsvToJson(file);
 
-        jsonData === null ? this.alertService.addError('Cargue un archivo .csv') : this.csvFileData = jsonData;
+        jsonData === null ? this.alertService.addError('Cargue un archivo .csv') : this.csvRecords = jsonData;
       } catch (error) {
         console.error('Error al leer el archivo csv:', error);
       }
@@ -448,5 +508,94 @@ export class DataSourceConnectionDetailPage implements OnInit {
     const data: any = await lastValueFrom(this.uploadFileService.upload(this.bigQueryFile(), '/global/upload/bigqueryCredentials'));
     this.bigQueryProjectId = data?.file?.project_id;
   }
+
+
+
+
+//REVISAR
+
+
+
+async onFilesAdded() {
+    const file = this.file.nativeElement.files[0];
+    try {
+      this.csvRecords = await this.ngxCsvParser.parse(file, { header: true, delimiter: this.delimiter })
+      .pipe().toPromise();
+      this.csvHeaders = Object.keys(this.csvRecords[0]);
+      const types = this.getTypes(this.csvHeaders, this.csvRecords);
+      this.csvHeaders.forEach((header, h) => {
+        let row = { field: header };
+        for (let i = 0; i < 3; i++) {
+          row[this.names[i]] = i === 0 ? types[h] : '';
+        }
+        this.csvColumns.push(row);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  getTypes(headers: Array<string>, data: Array<{}>) {
+    let max_rows = 10000;
+    let types = new Array(headers.length).fill('numeric');
+    let nulls = new Array(headers.length).fill(true);
+
+    if (max_rows > data.length) max_rows = data.length;
+    let onlyNumbers = /^\d+$/;
+
+    for (let i = 0; i < max_rows; i++) {
+      headers.forEach((header, j) => {
+        const value = data[i][header];
+        if (value && !onlyNumbers.test(value)) {
+          types[j] = 'text';
+          nulls[j] = false;
+        }else if(value){
+          nulls[j] = false;
+        }
+      });
+    }
+    types.forEach((type, i) => {
+      if(nulls[i]) types[i] = 'text';
+    });
+    return types;
+  }
+
+  loadFile() {
+    this.csvHeaders = [];
+    this.csvColumns = [];
+    this.file.nativeElement.value = "";
+    this.file.nativeElement.click();
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
