4328dd1b59a0f48396bbdf863a70454a90c7d071
MarcCornejoJort <marc.cornejo@jortilles.com>
2025-12-15 10:15:48 +0100

    calculated column dialog pasa a edadialog2

--- eda/eda_app/src/app/module/pages/data-sources/data-source-detail/calculatedColumn-dialog/calculated-column-dialog.component.ts
+++ eda/eda_app/src/app/module/pages/data-sources/data-source-detail/calculatedColumn-dialog/calculated-column-dialog.component.ts
@@ -1,5 +1,5 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, EventEmitter, Output } from '@angular/core';
-import { EdaDialogAbstract, EdaDialog, EdaDialogCloseEvent,EdaDialog2Component } from '@eda/shared/components/shared-components.index';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, EventEmitter, Input, Output } from '@angular/core';
+import { EdaDialogAbstract, EdaDialogCloseEvent,EdaDialog2Component, EdaDialog2 } from '@eda/shared/components/shared-components.index';
 import { AlertService} from '@eda/services/service.index';
 import { UntypedFormGroup, UntypedFormBuilder, Validators, ReactiveFormsModule, FormsModule} from '@angular/forms';
 
@@ -13,53 +13,31 @@ import { UntypedFormGroup, UntypedFormBuilder, Validators, ReactiveFormsModule, 
   imports: [ReactiveFormsModule, EdaDialog2Component, FormsModule]
 })
 
-export class CalculatedColumnDialogComponent extends EdaDialogAbstract {
+export class CalculatedColumnDialogComponent{
 
-  public dialog: EdaDialog;
-  public form: UntypedFormGroup;
+  public dialog: EdaDialog2;
 
-  constructor(
-    private formBuilder: UntypedFormBuilder,
-    private alertService: AlertService
-  ) {
-    super();
+  @Input() table: any;
+  @Input() controller: any;
+  @Output() close: EventEmitter<any> = new EventEmitter<any>();
+  
+  public columnName;
+  public descriptionContent;
+  public title = $localize`:@@addCalculatedColTitle:Añadir columna calculada a la tabla `;
 
-    this.dialog = new EdaDialog({
-      show: () => this.onShow(),
-      hide: () => this.onClose(EdaDialogCloseEvent.NONE),
-      title: $localize`:@@addCalculatedColTitle:Añadir columna calculada a la tabla `
-    });
-    this.dialog.style = { width: '50%', height: '40%', top:"-4em", left:'1em'};
-
-    this.form = this.formBuilder.group({
-      colName: [null, Validators.required],
-      description: [null, Validators.required]
-    });
-  }
-  onShow(): void {
-    const title = this.dialog.title;
-    this.dialog.title = `${title} ${this.controller.params.table.name}`;
-  }
-  onClose(event: EdaDialogCloseEvent, response?: any): void {
-    return this.controller.close(event, response);
-  }
-
-  closeDialog() {
-    this.onClose(EdaDialogCloseEvent.NONE);
-  }
-
+  constructor(private alertService: AlertService) {}
+    
   saveColumn() {
-    if (this.form.invalid) {
+    if (!this.validateForm()) {
       return this.alertService.addError($localize`:@@mandatoryFields:Recuerde llenar los campos obligatorios`);
     } else {
-
       const column: any = {
         aggregation_type: [{ value: "none", display_name: "No" }],
         column_granted_roles: [],
-        column_name: this.form.value.colName,
+        column_name: this.columnName,
         column_type: "numeric",
-        description: { default: this.form.value.description, localized: Array(0) },
-        display_name: { default: this.form.value.colName, localized: Array(0) },
+        description: { default: this.descriptionContent, localized: Array(0) },
+        display_name: { default: this.columnName, localized: Array(0) },
         row_granted_roles: [],
         SQLexpression: '',
         computed_column : 'computed',
@@ -70,6 +48,19 @@ export class CalculatedColumnDialogComponent extends EdaDialogAbstract {
     }
   }
 
+  public validateForm(): boolean{
+    let validatedForm = false;
+    if((this.columnName && this.columnName.trim() !== '') && (this.descriptionContent && this.descriptionContent.trim() !== '')){
+      validatedForm = true;
+    }
+    return validatedForm;
+  }
 
+  onClose(event: EdaDialogCloseEvent, response?: any): void {
+        return this.controller.close(event, response);
+    }
 
+  closeDialog() {
+    this.onClose(EdaDialogCloseEvent.NONE);
+  }
 }
