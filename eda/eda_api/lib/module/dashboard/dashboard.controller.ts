16360b4b73a4c0515f3b3e1978b09e4baa3e435c
Juanjo Ortilles <juanjo@jortilles.com>
2025-12-28 09:30:23 +0100

    json

--- eda/eda_api/lib/module/dashboard/dashboard.controller.ts
+++ eda/eda_api/lib/module/dashboard/dashboard.controller.ts
@@ -309,13 +309,13 @@ export class DashboardController {
       }
 
       /*Edalitics Free */ 
-      /*Edalitics Free */let str  =   '{ "_id": "new ObjectId(\"694fad8ddcb6050ca0213db8\")", ';
-      /*Edalitics Free */str += '"config": { "title": "Edalitics Sample Dasboard", "visible": "public","tag: "null","createdAt": "2025-12-27T10:42:55.432Z", "modifiedAt": "2025-12-27T11:06:14.116Z",';
-      /*Edalitics Free */str += ' "onlyIcanEdit": "false", "author": "edalitics@jortilles.com"  },  "user": {  "_id": "new ObjectId(\"694fad8c3515672426f53b40\")",';
+      /*Edalitics Free */let str  =   '{ "_id": "new ObjectId(694fad8ddcb6050ca0213db8)", ';
+      /*Edalitics Free */str += '"config": { "title": "Edalitics Sample Dasboard", "visible": "public","tag": "null","createdAt": "2025-12-27T10:42:55.432Z", "modifiedAt": "2025-12-27T11:06:14.116Z",';
+      /*Edalitics Free */str += ' "onlyIcanEdit": "false", "author": "edalitics@jortilles.com"  },  "user": {  "_id": "new ObjectId(694fad8c3515672426f53b40)",';
       /*Edalitics Free */str += ' "name": "edalitics@jortilles.com"} }';
 
-      /*Edalitics Free */const smple = JSON.parse(str);
-      /*Edalitics Free */shared.push(smple);
+      /*Edalitics Free */const sample = JSON.parse(str);
+      /*Edalitics Free */shared.push(sample);
       /*Edalitics Free */
 
       
