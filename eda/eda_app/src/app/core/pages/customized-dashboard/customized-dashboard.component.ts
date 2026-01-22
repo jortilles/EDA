3817815785ebff43059d9001a73ffcabd93723ab
Juanjo Ortilles <juanjo@jortilles.com>
2026-01-22 11:22:46 +0100

    cositas login y custom sidebar

--- eda/eda_app/src/app/core/pages/customized-dashboard/customized-dashboard.component.ts
+++ eda/eda_app/src/app/core/pages/customized-dashboard/customized-dashboard.component.ts
@@ -34,7 +34,7 @@ export class CustomizedDashboardComponent implements OnInit {
       {name: 'Delinqüència ', href: 'https://demo.edalitics.com/es/#/public/6801ea526c2a8f2c431e9b28' ,isStarting: false},
       {name: 'Viari ', href: 'https://demo.edalitics.com/es/#/public/6801ea526c2a8f2c431e9b28' ,isStarting: false},
       {name: 'Turisme ', href: 'https://demo.edalitics.com/es/#/public/6801ea526c2a8f2c431e9b28' ,isStarting: true, icon: 'sombrilla'},
-      {name: 'Visitants ', href: 'https://demo.edalitics.com/es/#/public/6801ea526c2a8f2c431e9b28://demo.edalitics.com/es/#/public/6801ea526c2a8f2c431e9b28' ,isStarting: false},
+      {name: 'Visitants ', href: 'https://demo.edalitics.com/es/#/public/6801ea526c2a8f2c431e9b28' ,isStarting: false},
       {name: 'Hosteleria ', href: 'https://demo.edalitics.com/es/#/public/6801ea526c2a8f2c431e9b28' ,isStarting: false},
       {name: 'Configuració ', href: 'https://demo.edalitics.com/es/#/public/6801ea526c2a8f2c431e9b28' ,isStarting: true, icon: 'engranaje'},
     ]
@@ -45,8 +45,8 @@ changeSrc(href: string) {
   console.log(href)
   const iframe = document.getElementById('showDashboard') as HTMLIFrameElement;
   if (iframe) {
-    const baseUrl = href;
-    iframe.src = baseUrl + '?refresh=' + Date.now();
+    const baseUrl = href + '?panelMode=true'
+    iframe.src = baseUrl + '&refresh=' + Date.now();
   }
 }
 
