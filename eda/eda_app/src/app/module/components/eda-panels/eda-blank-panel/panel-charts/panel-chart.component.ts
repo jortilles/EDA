1538f2ba8127f61060b22295ca7bdbe8f752181a
MarcCornejoJort <marc.cornejo@jortilles.com>
2026-01-23 13:31:28 +0100

    cambios scatterplot funcional

--- eda/eda_app/src/app/module/components/eda-panels/eda-blank-panel/panel-charts/panel-chart.component.ts
+++ eda/eda_app/src/app/module/components/eda-panels/eda-blank-panel/panel-charts/panel-chart.component.ts
@@ -982,20 +982,22 @@ private renderFunnel() {
     private renderScatter() {
 
         const dataDescription = this.chartUtils.describeData(this.props.query, this.props.data.labels);
-
         let inject: ScatterPlot = new ScatterPlot;
-        inject.size = this.props.size;
         inject.id = this.randomID();
+        inject.size = this.props.size;
         inject.data = this.props.data;
         inject.dataDescription = dataDescription;        
-        
-        const configColors = this.props.config.getConfig()['colors'];
-        inject.colors = (configColors && configColors.length > 0 && configColors) || this.chartUtils.generateChartColorsFromPalette(inject.data.values.length, this.paletaActual)
-            .map(item => item.backgroundColor);
-        inject.assignedColors = this.props.config.getConfig()['assignedColors'] || [];
-        //Tratamiento de assignedColors, cuando no haya valores, asignara un color        
-        this.props.config.setConfig(this.assignedColorsWork(this.props.config.getConfig(), inject));
         inject.linkedDashboard = this.props.linkedDashboardProps;
+        
+        const categoryIndex = dataDescription.otherColumns[0].index;
+        const categories = [...new Set(inject.data.values.map(row => row[categoryIndex]))];
+        
+        //Tratamiento de assignedColors, cuando no haya valores, asignara un color        
+        inject.assignedColors = this.chartUtils.resolveAssignedColors(
+            categories, this.props.config.getConfig()['assignedColors'] || [], this.paletaActual
+        );
+
+        this.props.config.setConfig({ ...this.props.config.getConfig(), assignedColors: inject.assignedColors });
 
         this.createScatter(inject);
     }
