5cb2014f648ddbc32376c4b86d3d39a56325072f
ronaldchavezjortilles <ronald.chavez@jortilles.com>
2026-04-29 11:50:54 +0200

    Refactory - Nuevo archivo de configuracion de IA

--- eda/eda_api/config/ai.config.js
+++ eda/eda_api/config/ai.config.js
@@ -1,7 +1,9 @@
-// ai.config.js
-module.exports = {
-    PROVIDER: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+module.exports = { 
+    PROVIDER: 'openai',
     API_KEY: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    AWS_ACCESS_KEY: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    AWS_SECRET_KEY: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    AWS_REGION: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
     MODEL: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
     CONTEXT: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
     AVAILABLE: true,
