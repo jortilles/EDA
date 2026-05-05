892660ca2edb9049b93557e753d37ae9805caeb8
MarcCornejoJort <marc.cornejo@jortilles.com>
2026-05-05 10:31:35 +0200

    Restructuración mcp

--- eda/eda_api/config/ai.config.js
+++ eda/eda_api/config/ai.config.js
@@ -9,7 +9,11 @@ module.exports = {
     AVAILABLE: false,
     LIMIT: 200,
     MAX_LIMIT: 300,
+    // Credenciales para el MCP integrado (/ia/mcp)
     MAX_TOKENS: 5000,
     EDA_APP_URL: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
     MCP_URL: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    //Usuario auxiliar de mcp
+    MCP_EMAIL:'XXXXX@XXXXX.com',
+    MCP_PASSWORD: 'XXXXXXXX',
 };
