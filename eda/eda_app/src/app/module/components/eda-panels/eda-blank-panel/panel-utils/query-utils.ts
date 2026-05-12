89e555e2e02a5cb0e943a5b7f5bbe89c8ac05212
ronaldchavezjortilles <ronald.chavez@jortilles.com>
2026-05-12 17:21:33 +0200

    Tabla cruzada con IA

--- eda/eda_app/src/app/module/components/eda-panels/eda-blank-panel/panel-utils/query-utils.ts
+++ eda/eda_app/src/app/module/components/eda-panels/eda-blank-panel/panel-utils/query-utils.ts
@@ -340,9 +340,9 @@ export const QueryUtils = {
         // Verifica un nuevo cambio en los Axes desde que se inicia la edición de la tabla cruzada
         if(!ebp.newAxesChanged && (!ebp.chartTypes.filter( grafico => grafico.subValue==='crosstable' )[0].ngIf || !ebp.chartTypes.filter( grafico => grafico.subValue==='table' )[0].ngIf)) {
 
-          if(ebp.currentQuery.length>2 && (ebp.currentQuery.find( valor => valor.column_type === 'numeric') !== undefined)) {
+          if(ebp.currentQuery.length>2 && (ebp.currentQuery.find( valor => valor.column_type === 'numeric') !== undefined) && ebp.panelChartConfig?.config) {
             const config = ebp.panelChartConfig.config.getConfig(); // Adquiera la configuración config
-            ebp.currentQuery = ebp.newCurrentQuery(ebp.currentQuery, ebp.initAxes(ebp.currentQuery)); // Reordeno el currentQuery                
+            ebp.currentQuery = ebp.newCurrentQuery(ebp.currentQuery, ebp.initAxes(ebp.currentQuery)); // Reordeno el currentQuery
             config['ordering'] = [{axes: ebp.initAxes(ebp.currentQuery)}]; // Agrego el nuevo axes a la config
             ebp.copyConfigCrossTable = JSON.parse(JSON.stringify(config));
           } 
