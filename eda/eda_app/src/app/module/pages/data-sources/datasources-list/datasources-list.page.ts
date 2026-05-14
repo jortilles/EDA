da05a31fc89d48277fa6dda4318de5c53ec1bf27
MarcCornejoJort <marc.cornejo@jortilles.com>
2026-05-14 11:40:51 +0200

    datasource list css

--- eda/eda_app/src/app/module/pages/data-sources/datasources-list/datasources-list.page.ts
+++ eda/eda_app/src/app/module/pages/data-sources/datasources-list/datasources-list.page.ts
@@ -14,6 +14,7 @@ import { Router } from '@angular/router';
   templateUrl: './datasources-list.page.html',
   standalone: true,
   imports: [SharedModule, CommonModule, FormsModule, IconComponent],
+  styleUrls: ['./datasources-list.page.css']
 })
 export class DataSourceListPage implements OnInit {
   private datasourceService = inject(DataSourceNamesService);
