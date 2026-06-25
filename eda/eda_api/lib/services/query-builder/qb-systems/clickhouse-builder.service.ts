31f899db43463a4fe4e7a3eb7bfa3b518f43e5e3
MarcCornejoJort <marc.cornejo@jortilles.com>
2026-06-25 11:43:37 +0200

    fix: corrección de la generación de queries en paneles ClickHouse

--- eda/eda_api/lib/services/query-builder/qb-systems/clickhouse-builder.service.ts
+++ eda/eda_api/lib/services/query-builder/qb-systems/clickhouse-builder.service.ts
@@ -153,20 +153,22 @@ export class ClickHouseBuilderService extends QueryBuilderService {
   ) {
     let o = tables.filter(t => t.name === origin).map(t => t.query ? this.cleanViewString(t.query) : t.name)[0];
     let vista = tables.filter(table => table.name === origin).map(table => { return table.query ? true: false })[0];
-    let myQuery: string;
+    let myQuery = `SELECT ${columns.join(', ')} \n`;
 
-
-    if (forSelector === true) {
-      if(vista){
-        myQuery = `SELECT DISTINCT ${columns.join(', ')} \nFROM ${o} `;
-      }else if(_schema){
-          myQuery = `SELECT DISTINCT ${columns.join(', ')} \nFROM \`${_schema}\`.\`${o}\` `;
-      }else{
-          myQuery = `SELECT DISTINCT ${columns.join(', ')} \nFROM \`${o}\``;
-      }
+    if (vista) {
+      myQuery += `FROM ${o}`;
+    } else {
+      myQuery += `FROM \`${o}\``;
     }
 
-
+    /** SI ES UN SELECT PARA UN SELECTOR  VOLDRÉ VALORS ÚNICS */
+    if (forSelector === true) {
+      if (vista) {
+        myQuery = `SELECT DISTINCT ${columns.join(', ')} \nFROM ${o}`;
+      } else {
+        myQuery = `SELECT DISTINCT ${columns.join(', ')} \nFROM \`${o}\``;
+      }
+    }
 
     // JOINS
     let joinString: any[];
